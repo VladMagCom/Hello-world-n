20fec85fb98a0e97fe324f924d3ab2e394e17d82
VladMagCom <furazikoff@gmail.com>
2023-06-16 22:33:55 +0300

    change cmake

---
+++ build/CMakeFiles/PrintHello.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/wlad/Desktop/материалы/prog/github/HelloWorld/main.cpp" "CMakeFiles/PrintHello.dir/main.cpp.o" "gcc" "CMakeFiles/PrintHello.dir/main.cpp.o.d"
+  "/Users/wlad/Desktop/материалы/prog/github/HelloWorld/print.cpp" "CMakeFiles/PrintHello.dir/print.cpp.o" "gcc" "CMakeFiles/PrintHello.dir/print.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PrintHello.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrintHello.dir/main.cpp.o"
+  "CMakeFiles/PrintHello.dir/main.cpp.o.d"
+  "CMakeFiles/PrintHello.dir/print.cpp.o"
+  "CMakeFiles/PrintHello.dir/print.cpp.o.d"
+  "PrintHello"
+  "PrintHello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PrintHello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
